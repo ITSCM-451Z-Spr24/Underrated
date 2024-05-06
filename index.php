8e0365f5648105d9c7913feb717c764bb330f873
logan allender <allenderlm30@uww.edu>
2024-05-05 21:29:19 -0500

    image

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $pageTitle = "Fantasy Baseball Predictive Model";
   <section class="hero">
     <h1>Find Hidden Gems: Fantasy Baseball All-Star Misfits</h1>
     <p>This website uses a predictive model to identify players with All-Star potential who were overlooked during the draft. 
-      Leverage our insights to build a winning fantasy team!</p>
+      Leverage our insights to build a winning fantasy team! </br> <img src="mp.png" alt="miller park" width="500" height="333">   </p>
   </section>
   </div>
 </main>
